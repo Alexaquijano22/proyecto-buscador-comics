f3d35e41a6c0b28dcbdfb16a200831380826110c
Alexandra Q <alex22quijano@gmail.com>
2021-10-09 20:21:52 -0500

    Primer commit

---
+++ types/types.ts
@@ -0,0 +1,51 @@
+type Characters = {
+    comics: Comics;
+    description: string;
+    events?: Events
+    id: number;
+    modified?: string,
+    name: string;
+    resourceURI?: string;
+    series?: Series;
+    stories?: {};
+    thumbnail?:{};
+    urls?:[];
+}
+
+type Comics ={
+    characters: Characters;
+    collectedIssues?: [];
+    collections?:[];
+    creators?:{};
+    dates?:[];
+    description?: string;
+    diamondCode?: string;
+    digitalID?: number;
+    ean?: string;
+    events?: {};
+    format?: string;
+    id?: number;
+    images?: [];
+    isbn?: string;
+    issn?: string;
+    issueNumber?: number;
+    modfied?: string;
+    pageCount?: number;
+    prices?: [{}];
+    resourceURI?: string;
+    series?: {};
+    stories?: {};
+    textObjects?: [];
+    thumbnail?: {};
+    title?: string;
+    upc?: string;
+    urls?: [{}];
+    variantDescription?: string;
+    variants?: []    
+}
+type Events ={
+
+}
+type Series = {
+
+}
